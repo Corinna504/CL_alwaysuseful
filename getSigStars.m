c27a4454b860eb99f59d4f8c55d666b8841bfb69
Corinna <corinn_lorenz@gmx.de>
2017-02-07 15:39:20 +0100

    adjusted for paper

--- getSigStars.m
+++ getSigStars.m
@@ -1,13 +1,32 @@
 
 function stars = getSigStars(p)
 
-if p < 0.001
-    stars = '***';
-elseif p<0.01
-    stars = '**';
-elseif p<0.05
-    stars = '*';
-else
-    stars = '';
+if p >=0 %parametric test
+    if p < 0.001
+        stars = '***_{p}';
+    elseif p<0.01
+        stars = '**_{p}';
+    elseif p<0.05
+        stars = '*_{p}';
+    elseif p<0.08
+        stars = 'o_{p}';
+    else
+        stars = ' _{p}';
+    end
+    
+elseif p<0 %non-parametric test
+    
+    if p > -0.001
+        stars = '***_{np}';
+    elseif p>-0.01
+        stars = '**_{np}';
+    elseif p>-0.05
+        stars = '*_{np}';
+    elseif p>-0.08
+        stars = 'o_{np}';
+    else
+        stars = ' _{np}';
+    end
+    
 end
 end
