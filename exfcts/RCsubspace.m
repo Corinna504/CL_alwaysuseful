c23b5afcc099abfa2c10d6dfc2fb7be10bad7cfb
Corinna <corinn_lorenz@gmx.de>
2017-02-07 15:35:30 +0100

    removed unnecessary code

--- exfcts/RCsubspace.m
+++ exfcts/RCsubspace.m
@@ -6,12 +6,16 @@ function [ res, mn_rate, fitparam ] = RCsubspace( ex, varargin )
 
 
 j=1; p_flag = false;    lat_flag = true;
+nsmpl = 1000;       % number of resampling processes  
+
 while j<= length(varargin)
    switch varargin{j}
        case 'plot'
            p_flag = varargin{j+1};
        case 'lat_flag'
            lat_flag = varargin{j+1};
+       case 'bootstrap_n'
+           nsmpl = varargin{j+1};
    end
    j=j+2;
 end
@@ -20,12 +24,12 @@ if p_flag; figure; end;
 ex.Trials = ex.Trials([ex.Trials.Reward]>0);
 
 % get errorbars by resampling
-nsmpl = 1000;       % number of resampling processes  
-[res, mn_rate] = resampleRC(ex, nsmpl, 'lat_flag', lat_flag);
+[res, mn_rate, bootsmpl] = resampleRC(ex, nsmpl, 'lat_flag', lat_flag);
 
 % fit tuning curves
-fitparam = fitOR(mn_rate, p_flag, res.sdfs.n);
+fitparam = fitOR2(mn_rate);
 
+mn_rate.bootstrap = bootsmpl;
 if size(res.sdfs.mn_rate(1).mn,1)>1 && size(res.sdfs.mn_rate(1).mn,2)>1
     
     % I am not sure here how to fit in 2D therefore I fit each orientation
@@ -34,7 +38,7 @@ if size(res.sdfs.mn_rate(1).mn,1)>1 && size(res.sdfs.mn_rate(1).mn,2)>1
         temp.mn     = res.sdfs.mn_rate(1).mn(:,i);
         temp.sem    = res.sdfs.mn_rate(1).sem(:,i);
         temp.or     = res.sdfs.x(:,1);
-        fitparam.others.OR(i) = fitOR(temp, p_flag, res.sdfs.n(:,i));
+        fitparam.others.OR(i) = fitOR2(temp);
     end
     
     corate = mean(res.sdfs.mn_rate(1).mn, 1);
@@ -46,64 +50,70 @@ if size(res.sdfs.mn_rate(1).mn,1)>1 && size(res.sdfs.mn_rate(1).mn,2)>1
     fitparam.others.CO = fitCO( corate, copar);
 end
 
-end
-
-
- %%
-function fitparam = fitOR(mn_rate, p_flag, n)
-
-mn = mn_rate.mn(mn_rate.or<180);
-sem = mn_rate.sem(mn_rate.or<180);
-or = mn_rate.or(mn_rate.or<180);
-
-
-%%% fit gauss shape
-offset = abs(min(mn));
-
-% CL fit
-[~, ~, val] = fitgauss( mn+offset, sem, or );
-
-% HN fit
-[fitHN,~,~,~,~,~]  = FitGauss_HN(val.uqang, val.mn, 'lin' );
-
-% CL fit with HN parameters
-[fitHN.mean, val.uqang] = unwrapdeg(fitHN.mean, val.uqang);
-[fitparam, gaussr2, val] = fitgauss( mn+offset, ...
-                                    sem, ...
-                                    or, fitHN, val );
-
-fitparam.b   = fitparam.b - offset;
-fitHN.base      = fitHN.base - offset;
-
-val.mn          = val.mn - offset;
-fitparam.val = val;
-fitparam.r2  = gaussr2;
-fitparam.HN  = fitHN;
 
 
 % plot results if necessary
 if p_flag
-%     hold on;
-    figure
-    x = fitparam.mu-100 : fitparam.mu+100;
-    y = gaussian(fitparam.mu, fitparam.sig, fitparam.a, fitparam.b,x);
-    plot(x, y, 'r--'); hold on;
+    subplot(2,2,1);
+    plot(fitparam.x, fitparam.y, 'r-'); hold on;
+    errorbar(fitparam.val.or, fitparam.val.mn, fitparam.val.sem, 'rx');
     
-    errorbar(fitparam.val.uqang, fitparam.val.mn, fitparam.val.sd); hold on;
+    subplot(2,2,[3 4]);
+    c = hsv(length(res.sdfs.s));
+    % stimulus triggered SDFs
+    for i = 1:length(res.sdfs.s) % orientation
+        plot(res.times/10, res.sdfs.s{i}, 'Color', c(i,:)); ho
+        leg{i} = sprintf('%1.0f \t n=%1.0f', res.sdfs.x(i), res.sdfs.n(i));
+    end
+    plot(res.times/10, res.sdfs.extras{1}.sdf, 'r:'); ho
+    legend(leg, 'Location', 'EastOutside');
     
+    s = horzcat(res.sdfs.s{:});
+    meanfr = mean(mean(s(201:400),2));
+    
+    latfp = res.latFP; 
+    lathmax = res.lat; 
+    dur = res.dur; 
+    
+    ylim_ = get(gca, 'ylim'); 
+    
+    plot([latfp latfp], ylim_, 'k');
+    plot([lathmax lathmax], ylim_, 'k--');
+    plot([lathmax+dur, lathmax+dur], ylim_, 'k');
+
+    
+    title(sprintf('lat: %1.1f (hmax %1.1f), dur: %1.1f, \n average sd: %1.2f, mean fr: %1.2f',...
+        res.latFP, res.lat, res.dur, mean(sqrt(res.vars(201:400))), meanfr));
+    xlim([0 160]); grid on;
+    ylabel('spk/s');
+
 end
 
+end
+
+
+
+ %%
+function fitparam = fitOR2(val)
+
+mn = val.mn(val.or<180)';
+sem = val.sem(val.or<180)';
+or = val.or(val.or<180)';
+
+% fit gaussian function to the mean values
+fitparam = fitOR( mn, sem, or);
 end
 
 
 %%
-function [res, mn_rate] = resampleRC(ex, nsmpl, varargin)
+function [res, mn_rate, bootsmpl] = resampleRC(ex, nsmpl, varargin)
 % randomly choose n trials and compute netspikes
 % do this nsmpl times
 
 res = HN_computeLatencyAndNetSpk([], ex, varargin{:});
 
 res_boot = cell(nsmpl,1); % results from bootstrapping samples
+
 
 parfor i = 1:nsmpl
     
@@ -116,12 +126,15 @@ parfor i = 1:nsmpl
     
     % use HN function to compute the usual res struct
     res_boot{i} = HN_computeLatencyAndNetSpk([], ex_boot, 'lat_flag', 0);
-    nspk(:,:,i) = res_boot{i}.netSpikesPerFrame;
 %     latfp_boot(i) = res_boot{i}.latFP;
 %     lathmax_boot(i) = res_boot{i}.lat;
+    bootsmpl(:,:,i) = res_boot{i}.netSpikesPerFrame;
+    
     
     if ~isempty(res.netSpikesPerFrameBlank)
         nspk_blank(i) = res_boot{i}.netSpikesPerFrameBlank(1);
+    else
+            
     end
 
 end
@@ -130,17 +143,17 @@ end
 % get the distribution information
 mn_rate = struct();
 % for orientation data
-for i1=1:size(nspk,1) % contrast
-    for i2=1:size(nspk,2) % orientation
-        res.sdfs.mn_rate(1).mn(i1,i2) = mean(nspk(i1, i2, :));
-        res.sdfs.mn_rate(1).sem(i1,i2) = std(nspk(i1, i2, :));
+for i1=1:size(bootsmpl,1) % contrast
+    for i2=1:size(bootsmpl,2) % orientation
+        res.sdfs.mn_rate(1).mn(i1,i2) = mean(bootsmpl(i1, i2, :));
+        res.sdfs.mn_rate(1).sem(i1,i2) = std(bootsmpl(i1, i2, :));
     end
 end
 mn_rate.or= res.sdfs.x(:,1); 
 [~, maxi]= max(res.sdfs.y(1,:));
 
 mn_rate.mn  = res.netSpikesPerFrame(:, maxi);
-mn_rate.sem = std( nspk, 0, 3  );
+mn_rate.sem = std( bootsmpl, 0, 3  );
 
 
 %for blanks
