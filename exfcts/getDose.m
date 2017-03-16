6d8ef7a7dd9b30079ee48d5da57f848ecec0e17f
Corinna <corinn_lorenz@gmx.de>
2017-03-16 12:29:02 +0100

    bin the dose

--- exfcts/getDose.m
+++ exfcts/getDose.m
@@ -1,13 +1,41 @@
-function dose = getDose(ex)
+function [dose, dosernd] = getDose(ex)
         dose = -1;
 
 if isfield(ex.Header, 'Headers')
     if isfield(ex.Header.Headers(1), 'iontophoresisEjectionCurrent')
-        dose = ex.Header.Headers(1).iontophoresisEjectionCurrent;
+        dose(1) = ex.Header.Headers(1).iontophoresisEjectionCurrent;
     end
 else
     if isfield(ex.Header, 'iontophoresisEjectionCurrent')
-        dose = ex.Header.iontophoresisEjectionCurrent;
+        dose(1) = ex.Header.iontophoresisEjectionCurrent;
     end
 end
+dosernd = getRoundDose(dose(1));
+end
+
+
+function dosernd = getRoundDose(dose)
+
+
+if dose<0
+    dosernd=-1;
+elseif dose==0.1
+    dosernd=-1;
+elseif dose<=5
+    dosernd=5;
+elseif dose<=10
+    dosernd=10;    
+elseif dose<=16
+    dosernd=16;    
+elseif dose<=25
+    dosernd=25;
+elseif dose<=35
+    dosernd = 35;
+elseif dose<=inf
+    dosernd = 45;
+else
+    dosernd = dose;
+end
+
+
 end
